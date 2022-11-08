50065ef948f672abed4706fd965eb20a0031960b
my <264734208@qq.com>
2022-11-08 03:17:56 +0000

    add syscall table

--- trace.py
+++ trace.py
@@ -6,6 +6,9 @@ import argparse
 import json
 from bcc import BPF
 from ctypes import *
+import sys_arm32
+import sys_arm64
+
 class SysDesc(Structure):
     _fields_ = [("nArgs", c_int32),
             ("stringMask", c_int32),
@@ -14,13 +17,22 @@ class SysDesc(Structure):
 if __name__ == "__main__":
     b = BPF(src_file="trace.c")
     print("after compile")
-    syscall = {}
     tbl = b["sysdesc"]
-    print(tbl)
-    print(dir(tbl))
-    syscallNameTp = c_char * 50
-    print(syscallNameTp)
-    sysval = SysDesc(c_int32(2), c_int32(1<<1), b"faccessat")
-    tbl[c_int(48)] = sysval
+
+    systbl = sys_arm64.g_systbl
+
+    for sysId in systbl:
+        sysUserDesc = systbl[sysId]
+        nArgs = sysUserDesc[0]
+        syscallName = sysUserDesc[1].encode("utf-8")
+        sysMask = 0
+        n = len(sysUserDesc)
+        if (n > 2):
+            sysMask = sysUserDesc[2]
+        sysval = SysDesc(c_int32(nArgs), c_int32(sysMask), syscallName)
+        tbl[c_int(sysId)] = sysval
+    #
+    print("ready...")
+
     b.trace_print()
 #
